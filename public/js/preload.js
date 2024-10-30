3f8d51dd5552c29ca6954496be1dec0652f00a16
Maximilian Benner <maxibenner@gmail.com>
2024-10-30 16:13:41 -0400

    feat: added instant load library

--- public/js/preload.js
+++ public/js/preload.js
@@ -1,27 +1,39 @@
-// Track preloaded pages
-const preloadedPages = new Array();
+// // Track preloaded pages
+// const preloadedPages = new Array();
 
-// Get all links on the page
-const links = document.querySelectorAll("a");
+// // Get all links on the page
+// const links = document.querySelectorAll("a");
 
-// Add hover event listener to each link
-links.forEach((link) => {
-  link.addEventListener("mouseover", (event) => {
-    event.preventDefault();
+// // Add hover event listener to each link
+// links.forEach((link) => {
+//   // Desktop
+//   link.addEventListener("mouseover", (event) => {
+//     event.preventDefault();
+//     preload(link.href);
+//   });
+//   // Mobile
+//   link.addEventListener("touchstart", (event) => {
+//     event.preventDefault();
+//     preload(link.href);
+//   });
+// });
 
-    // Preload the url
-    preload(link.href);
-  });
-});
+// async function preload(url) {
+//   // Only preload if the page hasn't been preloaded yet
+//   if (!preloadedPages.includes(url)) {
+//     // Push to tracking array
+//     preloadedPages.push(url);
 
-async function preload(url) {
-  // Only preload if the page hasn't been preloaded yet
-  if (!preloadedPages.includes(url)) {
-    // Fetch the page
-    await fetch(url, { method: "GET", mode: "no-cors" });
+//     // Fetch the page
+//     const res = await fetch(url, { method: "GET", mode: "no-cors" });
+//     const data = await res.text();
 
-    // Push to tracking array
-    preloadedPages.push(url);
-    console.log(`Preloaded: ${url}`);
-  }
-}
+//     // Extract the body from the response
+//     const bodyNew = data.match(/<body[^>]*>([\s\S]*?)<\/body>/i)[1];
+
+//     // Replace current body with the new one
+//     document.body.innerHTML = bodyNew;
+
+//     console.log(`Preloaded: ${url}`);
+//   }
+// }
